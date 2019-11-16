43a7cf1d1e5cb41c931beac79c934baa11fed7b5
Miguel Perez Andrade <miguel.perezandrade@gmail.com>
2019-11-16 16:56:04 -0300

    simulación funcionando, queda ver porque da mal

--- wire/TpVsN.py
+++ wire/TpVsN.py
@@ -1,28 +1,33 @@
 from wire.Circuit import Circuit
 from wire.Simulation import Simulation
 from wire.Results import Results
+import matplotlib.pyplot as plt
 
 
 # definimos las variables
 C = 10e-6
 R = 10e3
-N_max = 15
-end_time = 1
+N_max = 40
+end_time = 100
 time_step = end_time/1e3
-
-# creamos los objetos
-circuit = Circuit(R, C)
-simulation = Simulation(time_step, end_time, circuit)
-results = Results()
+T_vec = []
+N_vec = []
 
 for N in [N+1 for N in range(N_max)]:
-    # Modificamos N
-    circuit.N = N
+
+    # creamos los objetos
+    circuit = Circuit(R, C, N)
+    simulation = Simulation(time_step, end_time, circuit)
+    results = Results()
+
     # simulamos y procesamos los resultados
     simulation.run(results.filename)
     results.process()
-    results.plot()
-    print('El rise time es de {rise_time}'.format(rise_time=results.get_rise_time()))
+    T_vec.append(results.get_rise_time())
+    N_vec.append(N)
 
-exit()
+plt.plot(N_vec, T_vec)
+plt.ylabel('Tiempo de crecimiento [s]')
+plt.xlabel('Ctdad de cuadripolos')
+plt.show()
 
