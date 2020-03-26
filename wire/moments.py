2c4178eb6000b99e094cb8de39699a666230fece
Miguel Perez Andrade <miguel.perezandrade@gmail.com>
2020-03-26 00:37:24 -0300

    calcula bien los momentos, queda documentar y convertir en función

--- wire/moments.py
+++ wire/moments.py
@@ -10,29 +10,17 @@ R = 1
 C = 1
 
 state_vector_length = N+2
+moments = np.zeros((state_vector_length, M+1))
 
-# m0 es un vector de unos salvo un cero en la última posición
-m0 = np.ones(state_vector_length)
-m0[len(m0)-1] = 0
+# Los momentos de órden 1 son tensiones unitarias y corriente nula
+moments[:state_vector_length-1, 0] = np.ones(state_vector_length-1
 
-# m1 es más turbio
-m1 = np.zeros(state_vector_length)
-m1[0] = 0
-m1[1] = -N*R
-m1[len(m1)-1] = -N
-for i in range(2, len(m1)-1):
-    m1[i] = m1[i-1] - (N+1-i)*R
+for order in range (1, M+1):
+    # El último nodo es la corriente por la fuente de tensión, la suma con signo 
+    # negativo de todas las corrientes de las fuentes de corriente
+    moments[state_vector_length-1, order] = -np.sum(moments[1:N+1, order-1])*C
+    for i in range(1, state_vector_length-1):   
+        moments[i, order] = moments[i-1, order] - R*np.sum(moments[i:N+1, order-1])*C
 
-# m2, otro hijo de puta
-m2 = np.zeros(state_vector_length)
-# El nodo 0 está a tierra
-m2[0] = 0
-# El último nodo es la corriente por la fuente de tensión, la suma con signo 
-# negativo de todas las corrientes de las fuentes de corriente
-m2[len(m2)-1] = -np.sum(m1[1:N+1])
-for i in range(1, len(m2)-1):   
-    m2[i] = m2[i-1] - np.sum(m1[i:N+1])*R
 
-print(m0)
-print(m1)
-print(m2)
+print(moments)
